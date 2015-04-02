cbe2b86914815e6de1812c54b96ebf035320d0c1
Jim Kang <jimkang@gmail.com>
2015-04-02 15:39:49 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,47 @@
+simplescroll
+============
+
+A very simple inertial scrolling module that puts together easing functions and a timer to smoothly scroll the page to where you want it.
+
+Installation
+------------
+
+    npm install simplescroll
+
+Usage
+-----
+
+Via script tags:
+
+    <script src="simplescroll.js"></script>
+    var simpleScroll = createSimpleScroll({
+      d3: d3
+    });
+    simpleScroll.scrollToElement(document.querySelect('#the-goal'), 500);
+
+Via Browserify:
+
+  var createSimpleScroll = require('simplescroll');
+  var simpleScroll = createSimpleScroll({
+    d3: require('d3') // Or your custom build of D3.
+  });
+  simpleScroll.scrollToElement(document.querySelect('#the-goal'), 500);
+
+You can also specify an [easing type](https://github.com/mbostock/d3/wiki/Transitions#d3_ease):
+
+  var simpleScroll = createSimpleScroll({
+    d3: require('d3'),
+    easingType: 'bounce'
+  });
+
+Example
+-------
+
+[Here's an example.](http://jimkang.com/simplescroll/example)
+
+And [here's an explanation](bl.ocks.org/jimkang/e318dfad9c798a456ded) about how it works.
+
+License
+-------
+
+MIT.
---
+++ simplescroll.js
@@ -0,0 +1,62 @@
+var commonJS = (typeof module === 'object' && typeof module.exports === 'object');
+
+function createSimpleScroll(opts) {
+  var localD3;
+  var easingType;
+
+  if (opts) {
+    localD3 = opts.d3;
+    easingType = opts.easingType;
+  }
+
+  if (!localD3 && !commonJS) {
+    // Probably being included via script tags. Try the global D3.
+    localD3 = d3;
+  }
+
+  if (!easingType) {
+    easingType = 'cubic';
+  }
+
+  var easingFn = localD3.ease(easingType);
+
+  function scrollTo(toTop, time) {
+    var fromTop = document.body.scrollTop;
+    var scrollDistance = toTop - fromTop;
+    var scrolledTop = 0;
+
+    localD3.timer(updateScrollTop);
+
+    function updateScrollTop(elapsed) {
+      var portion = easingFn(elapsed * 1.0 /time);
+      var scrollChange = scrollDistance * portion;
+
+      document.body.scrollTop = fromTop + scrollChange;
+
+      // Stop the timer by returning true if we've scrolled as far as requested.
+      if (scrollDistance < 0 && document.body.scrollTop <= toTop) {
+        return true;
+      }
+      if (scrollDistance >= 0 && document.body.scrollTop >= toTop) {
+        return true;
+      }
+      if (elapsed > time) {
+        // This is as far as we're going to get.
+        return true;
+      }
+    }
+  }
+
+  function scrollToElement(el, time) {
+    scrollTo(el.offsetTop, time);
+  }
+
+  return {
+    scrollTo: scrollTo,
+    scrollToElement: scrollToElement
+  };
+}
+
+if (commonJS) {
+  module.exports = createSimpleScroll;
+}
